3fa30dbe030819b579afb619d4a1b67f493aa04d
jpdias <jpdias@live.com.pt>
2013-09-13 00:17:41 +0100

    Almost done.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -28,15 +28,19 @@ namespace TrigMeter
 
 
         Point[] pontos = new Point[3];
-
+        double alfa, beta;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             float a = 0, b = 0, c = 0;
 
+
             if (C.Text.Trim().Length == 0)
             {
+              
                 a = float.Parse(A.Text);
                 b = float.Parse(B.Text);
+                alfa = a;
+                beta = b;
                 //c = float.Parse(C.Text);
                c = (float)Math.Sqrt(a * a + b * b);
                 C.Text = c.ToString();
@@ -44,8 +48,19 @@ namespace TrigMeter
             }
             else if (A.Text.Trim().Length == 0)
             {
+               
                 c = float.Parse(C.Text);
                 b = float.Parse(B.Text);
+                alfa = c;
+                beta = b;
+                if (b > c)
+                {
+                    MessageBox.Show("Cathethus can't be larger than hypotenuse!", "", MessageBoxButton.OK);
+                    clear();
+                    return;
+                }
+
+
                 //c = float.Parse(C.Text);
                 a = (float)Math.Sqrt(c * c - b * b);
                 A.Text =a.ToString();
@@ -53,8 +68,18 @@ namespace TrigMeter
            }
             else if (B.Text.Trim().Length == 0)
             {
+               
                 c = float.Parse(C.Text);
                 a = float.Parse(A.Text);
+                alfa = c;
+                beta = a;
+
+                if (a > c)
+                {
+                    MessageBox.Show("Cathethus can't be larger than hypotenuse!", "", MessageBoxButton.OK);
+                    clear();
+                    return;
+                } 
                 //c = float.Parse(C.Text);
                 b = (float)Math.Sqrt(c * c - a * a);
                 B.Text =b.ToString();
@@ -69,12 +94,14 @@ namespace TrigMeter
                 clear();
                 MessageBox.Show("One box must be empty!", "", MessageBoxButton.OK);
                 clear();
+     
             }
-            Draw.Children.Clear();
+           
            
             drw();
         }
 
+       
         private double DTR(double angle)
         {
             return (Math.PI * angle) / 180.0;
@@ -87,7 +114,7 @@ namespace TrigMeter
             c2 = RTD(Math.Acos(((float)(-b * b + c * c + a * a) / (float)(2 * a * c)))).ToString();
             C1.Text = c1.Substring(0, 8);
             C2.Text = c2.Substring(0, 8);
-            points(a, b, c, Math.Acos(((b * b + c * c - a * a) /(2 * b * c))), Math.Acos(((-b * b + c * c + a * a) / (2 * a * c))));
+            points( Math.Acos(((b * b + c * c - a * a) /(2 * b * c))), Math.Acos(((-b * b + c * c + a * a) / (2 * a * c))));
         }
         private void clear()
         {
@@ -96,29 +123,38 @@ namespace TrigMeter
             C.Text = "";
             C1.Text = "";
             C2.Text = "";
+            Draw.Children.Clear();
         }
-        public void points(float a, float b, float c, double ang1,double ang2)
+        public void points( double ang1,double ang2)
         {
+            double fact=1;
             Point[] pnt = new Point[3];
 
-            pnt[0].X = 200;
-            pnt[0].Y = 250;
+            pnt[0].X = 0;
+            pnt[0].Y = 356;
+
+            if ((pnt[0].Y - Math.Sin(Math.PI / 2) * alfa) < 50 || (pnt[1].X + Math.Cos(Math.PI / 4) * beta * fact) < 50)
+                fact = 2;
+            else if (((pnt[0].Y - Math.Sin(Math.PI / 2) * alfa) > 320 || (pnt[1].X + Math.Cos(Math.PI / 4) * beta * fact) > 320) 
+                && (pnt[0].Y - Math.Sin(Math.PI / 2) * alfa)<640 || (pnt[1].X + Math.Cos(Math.PI / 4) * beta * fact) < 640)
+                fact = 1 / 2;
+            else
+                fact = 1 / 5;
 
 
-
-            pnt[1].X = (int)(pnt[0].X - Math.Cos(ang2)*a);
-            pnt[1].Y =(int) (pnt[0].Y - Math.Sin(ang2)*a);
+            pnt[1].X = 0;// (int)(pnt[0].X - Math.Cos(Math.PI / 2) * alfa * 2);
+            pnt[1].Y = (int)(pnt[0].Y - Math.Sin(Math.PI / 2) * alfa * fact);
       
-          pnt[2].X = (int) (pnt[1].X - Math.Cos(ang1)*b);
-          pnt[2].Y = (int) (pnt[1].Y - Math.Sin(ang1)*b);
+          pnt[2].X = (int) (pnt[1].X + Math.Cos(Math.PI/4)*beta*fact);
+          pnt[2].Y = 356;// (int)(pnt[1].Y + Math.Sin(Math.PI / 4) * beta * 2);
 
           pontos = pnt;
 
-
+            
         }
         private void drw()
         {
-            
+            Draw.Children.Clear();
             
             Line line = new Line();
             Line line1 = new Line();
@@ -139,13 +175,13 @@ namespace TrigMeter
 
             line1.X1 = pontos[1].X;
             line1.X2 = pontos[2].X;
-            line1.Y1 = pontos[0].Y; 
-            line1.Y2 = pontos[1].Y;
+            line1.Y1 = pontos[1].Y; 
+            line1.Y2 = pontos[2].Y;
 
             line2.X1 = pontos[2].X;
-            line2.X2 = pontos[1].X;
-            line2.Y1 = pontos[0].Y;
-            line2.Y2 = pontos[1].Y;
+            line2.X2 = pontos[0].X;
+            line2.Y1 = pontos[2].Y;
+            line2.Y2 = pontos[0].Y;
 
             Draw.Children.Add(line);
             Draw.Children.Add(line1);
@@ -162,6 +198,11 @@ namespace TrigMeter
         }
 
         private void Panorama_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
